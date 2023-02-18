24f8e8d8975960d273318e026b23f3ba309b8ac1
Felix <felix_gilioli@hotmail.com>
2023-02-18 18:40:55 -0300

    testes de arquitetura

--- src/main/java/br/com/felixgilioli/alunoservice/service/AlunoService.java
+++ src/main/java/br/com/felixgilioli/alunoservice/service/AlunoService.java
@@ -2,12 +2,17 @@ package br.com.felixgilioli.alunoservice.service;
 
 import br.com.felixgilioli.alunoservice.entity.Aluno;
 import br.com.felixgilioli.alunoservice.repository.AlunoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public record AlunoService(AlunoRepository alunoRepository) {
 
+    private static final Logger log = LoggerFactory.getLogger(AlunoService.class);
+
     public Aluno salvar(Aluno aluno) {
+        log.info("buscando aluno no banco de dados");
         return alunoRepository.save(aluno);
     }
 }
