710bb7ed478eec1eb7f024e8569a3dd2d1013f0e
Felix <felix_gilioli@hotmail.com>
2023-02-24 23:36:32 -0300

    buscar por id

--- src/main/java/br/com/felixgilioli/alunoservice/controller/AlunoController.java
+++ src/main/java/br/com/felixgilioli/alunoservice/controller/AlunoController.java
@@ -2,6 +2,9 @@ package br.com.felixgilioli.alunoservice.controller;
 
 import br.com.felixgilioli.alunoservice.entity.Aluno;
 import br.com.felixgilioli.alunoservice.service.AlunoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +16,11 @@ public record AlunoController(AlunoService alunoService) {
         return alunoService.salvar(aluno);
     }
 
+    @Operation(description = "Busca o aluno pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o aluno"),
+            @ApiResponse(responseCode = "400", description = "Não existe o aluno com o id informado")
+    })
     @GetMapping("/{id}")
     public Aluno buscarPorId(@PathVariable Long id) {
         return alunoService.buscarPorId(id);
